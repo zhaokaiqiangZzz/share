c2cb3f91b30dc2ab10ab03f2ec6f13083b263c0e
zhaokaiqiangZzz <2391945695@qq.com>
2021-06-08 13:34:18 +0800

    前台帖子c层

---
+++ webApp/src/app/high-entrance/high-entrance-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HighEntranceComponent } from './high-entrance.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { ViewComponent } from './view/view.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HighEntranceComponent,
+    data: {
+      title: '列表'
+    }
+  },
+  {
+    path: 'add',
+    component: AddComponent,
+    data: {
+      title: '新增'
+    }
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent,
+    data: {
+      title: '编辑'
+    }
+  },
+  {
+    path: 'view/:id',
+    component: ViewComponent,
+    data: {
+      title: '查看'
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HighEntranceRoutingModule { }
